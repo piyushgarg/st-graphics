d51fe1fa64fb5f8652fc3569f1ddf2a4012401c3
toki83 <toki83w@gmail.com>
2025-08-08 18:43:31 +0200

    [CONFIG] Noto font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "NotoSansM NFP:size=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /* How to align the content in the window when the size of the terminal
